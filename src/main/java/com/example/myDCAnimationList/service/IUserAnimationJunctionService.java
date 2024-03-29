c9b921583298ae4b623570884c4b1dc285fdadaf
gyulaieric <eric.gyulai@gmail.com>
2024-03-29 15:50:14 +0200

    rewrote JunctionController and UserAnimationJunctionService

--- src/main/java/com/example/myDCAnimationList/service/IUserAnimationJunctionService.java
+++ src/main/java/com/example/myDCAnimationList/service/IUserAnimationJunctionService.java
@@ -1,13 +1,14 @@
 package com.example.myDCAnimationList.service;
 
 import com.example.myDCAnimationList.model.UserAnimationJunction;
+import org.springframework.security.core.Authentication;
 
 import java.util.List;
 import java.util.Map;
 
 public interface IUserAnimationJunctionService {
     List<UserAnimationJunction> getList(Long userId);
-    Map<String, String> saveJunction(Long userId, Long animationId);
-    void updateUserRating(Long id, UserAnimationJunction listItem);
-    void deleteJunction(Long userId, Long animationId);
+    Map<String, String> saveJunction(Authentication authentication, Long animationId);
+    Map<String, String> updateUserRating(Authentication authentication, Long id, UserAnimationJunction listItem);
+    void deleteJunction(Authentication authentication, Long animationId);
 }
--- src/main/java/com/example/myDCAnimationList/service/UserAnimationJunctionService.java
+++ src/main/java/com/example/myDCAnimationList/service/UserAnimationJunctionService.java
@@ -3,6 +3,8 @@ package com.example.myDCAnimationList.service;
 import com.example.myDCAnimationList.model.UserAnimationJunction;
 import com.example.myDCAnimationList.repository.AnimationRepository;
 import com.example.myDCAnimationList.repository.UserAnimationJunctionRepository;
+import com.example.myDCAnimationList.repository.UserRepository;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 import java.util.Collections;
@@ -13,10 +15,12 @@ import java.util.Map;
 public class UserAnimationJunctionService implements IUserAnimationJunctionService{
 
     private final UserAnimationJunctionRepository junctionRepository;
+    private final UserRepository userRepository;
     private final AnimationRepository animationRepository;
 
-    public UserAnimationJunctionService(UserAnimationJunctionRepository junctionRepository, AnimationRepository animationRepository) {
+    public UserAnimationJunctionService(UserAnimationJunctionRepository junctionRepository, UserRepository userRepository, AnimationRepository animationRepository) {
         this.junctionRepository = junctionRepository;
+        this.userRepository = userRepository;
         this.animationRepository = animationRepository;
     }
 
@@ -26,9 +30,9 @@ public class UserAnimationJunctionService implements IUserAnimationJunctionServi
     }
 
     @Override
-    public Map<String, String> saveJunction(Long userId, Long animationId) {
+    public Map<String, String> saveJunction(Authentication authentication, Long animationId) {
 
-        UserAnimationJunction userAnimationJunction = new UserAnimationJunction(userId, animationId);
+        UserAnimationJunction userAnimationJunction = new UserAnimationJunction(userRepository.findByUsername(authentication.getName()).get().getId(), animationId);
 
         if (isAnimationInList(userAnimationJunction) == null) {
             if (animationRepository.existsById(animationId)) {
@@ -43,19 +47,27 @@ public class UserAnimationJunctionService implements IUserAnimationJunctionServi
     }
 
     @Override
-    public void updateUserRating(Long id, UserAnimationJunction listItem) {
+    public Map<String, String> updateUserRating(Authentication authentication, Long id, UserAnimationJunction listItem) {
         UserAnimationJunction userAnimationJunction = junctionRepository.findById(id).orElseThrow(
                 () -> new IllegalStateException(String.format("List item with id %s doesn't exist", id))
         );
 
-        userAnimationJunction.setUserRating(listItem.getUserRating());
-        junctionRepository.save(userAnimationJunction);
+        if (listItem.getUserRating() > 10 || listItem.getUserRating() < 0) {
+            return Collections.singletonMap("error", "Invalid rating");
+        }
+        if (userRepository.findByUsername(authentication.getName()).get().getId().equals(junctionRepository.findById(id).get().getUserId())) {
+            userAnimationJunction.setUserRating(listItem.getUserRating());
+            junctionRepository.save(userAnimationJunction);
+            return Collections.singletonMap("response", "Successfully updated rating.");
+        } else {
+            return Collections.singletonMap("error", "You are trying to rate an animation in someone else's list.");
+        }
     }
 
     @Override
-    public void deleteJunction(Long userId, Long animationId) {
+    public void deleteJunction(Authentication authentication, Long animationId) {
 
-        UserAnimationJunction userAnimationJunction = isAnimationInList(new UserAnimationJunction(userId, animationId));
+        UserAnimationJunction userAnimationJunction = isAnimationInList(new UserAnimationJunction(userRepository.findByUsername(authentication.getName()).get().getId(), animationId));
 
         if (userAnimationJunction != null) {
             junctionRepository.deleteById(userAnimationJunction.getId());
